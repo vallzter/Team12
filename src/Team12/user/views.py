8047d6b1e6f4c54cccec21837cd47c00d80e174f
elmarolafsson <elmarolafsson@hotmail.com>
2020-09-04 15:55:22 +0000

    register

--- src/Team12/user/views.py
+++ src/Team12/user/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
-def home(request):
-    return render(request, 'user/register.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("register")
+    else:
+        form = UserCreationForm()
+
+        args = {"form": form}
+        return render(request, "user/register.html", args)
