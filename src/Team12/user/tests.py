c8860eafe8a5aa96974a338f3a657bedb8d6e9ad
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2020-09-05 15:19:23 +0000

    Update tests.py

--- src/Team12/user/tests.py
+++ src/Team12/user/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, Client
+from user import views
 
 # Create your tests here.
+
+class FrontPageTest(SimpleTestCase):
+
+    #Experimental test
+    def test_code(self): #This should return as false until we have a homepage
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_login(self):
+        testClient = Client()
+        isLogged = testClient.post('/login/', {'username': 'john', 'password': 'smith'})
+        isLogged = testClient.get('/customer/details/')
+        print(isLogged.content)
+        #self.assertEqual(isLogged.status_code, 200)
+
+    
