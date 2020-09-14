953ffac6e727db330dd46f37415100a0968a83f0
Garpur3 <garpurhnefill@gmail.com>
2020-09-14 22:44:55 +0000

    Added meal_package and one entry to db

---
+++ src/Team12/meal_package/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here.
+from django.db.models import ManyToManyField
+
+
+class Meal_package(models.Model):
+    name = models.CharField(max_length=32, blank=False)
+    ingredients = models.TextField(blank=False, default="N/A")
+    price = models.IntegerField(blank=False)
+    recipe = models.TextField(blank=False)
