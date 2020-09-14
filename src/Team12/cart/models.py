9e44db830390967da440ba2541115909c362c17e
fannarl <12133432+fannarl@users.noreply.github.com>
2020-09-14 22:36:20 +0000

    models for product and cart

--- src/Team12/cart/models.py
+++ src/Team12/cart/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class Cart_item(models.Model):
+    product = models.ForeignKey(Product,
+                                null=True,
+                                on_delete=models.SET_NULL)
+    quantity = models.IntegerField(default=1)
+    cart = models.ForeignKey('Cart',
+                             on_delete=models.CASCADE)
+
+class Contact_info(models.Model):
+    last_name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    country = CountryField()
+    address = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    postcode = models.CharField(max_length=10)
 # Create your models here.
--- src/Team12/product/models.py
+++ src/Team12/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    keywords = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
