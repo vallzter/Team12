7202e727b3574638afd3851b55b7d8498a2fd00a
fannarl <12133432+fannarl@users.noreply.github.com>
2020-09-16 11:49:29 +0000

    models for users

--- src/Team12/cart/models.py
+++ src/Team12/cart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Cart_item(models.Model):
+    user = models.ForeignKey(user, null=True, blank=True)
     product = models.ForeignKey(Product,
                                 null=True,
                                 on_delete=models.SET_NULL)
@@ -8,11 +9,11 @@ class Cart_item(models.Model):
     cart = models.ForeignKey('Cart',
                              on_delete=models.CASCADE)
 
-class Contact_info(models.Model):
-    last_name = models.CharField(max_length=64)
-    first_name = models.CharField(max_length=64)
-    country = CountryField()
-    address = models.CharField(max_length=128)
-    city = models.CharField(max_length=128)
-    postcode = models.CharField(max_length=10)
+# class Contact_info(models.Model):
+#    last_name = models.CharField(max_length=64)
+#    first_name = models.CharField(max_length=64)
+#    country = CountryField()
+#    address = models.CharField(max_length=128)
+#    city = models.CharField(max_length=128)
+#    postcode = models.CharField(max_length=10)
 # Create your models here.
--- src/Team12/user/models.py
+++ src/Team12/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class User(models.Model):
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    address = models.CharField(max_length=255, blank=True)
+    email = models.EmailField(max_length=70, blank=True)
+    zip_code = models.IntegerField()
+    phone_nr = models.IntegerField()
 # Create your models here.
