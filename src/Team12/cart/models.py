28a5a883acc3f2e94e5c17d762e81336ce8e9b65
fannarl <12133432+fannarl@users.noreply.github.com>
2020-09-16 11:59:28 +0000

    Update models.py

--- src/Team12/cart/models.py
+++ src/Team12/cart/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from src.Team12.product.models import Product
+
+
 class Cart_item(models.Model):
-    user = models.ForeignKey(user, null=True, blank=True)
+    user = models.ForeignKey(User, null=True, blank=True)
     product = models.ForeignKey(Product,
                                 null=True,
                                 on_delete=models.SET_NULL)
