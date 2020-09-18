0e9b217c985ea80445762e5877f79605333913c7
elmarolafsson <elmarolafsson@hotmail.com>
2020-09-18 21:59:50 +0000

    ahh

--- src/Team12/cart/views.py
+++ src/Team12/cart/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.contrib import messages
 from product.models import MealPlan
 from user.models import Customer
 from cart.models import LineItem, Cart
@@ -12,5 +14,28 @@ def index(request):
 # Create your views here.
 def add(request):
     # add item to cart
+    print("im here")
     if request.method == "GET":
-        pass #todo, add item to cart
+        raise Http404()
+    prod_id = request.POST.get('id')
+    print(prod_id)
+    print("im here")
+    quantity = request.POST.get('quantity') or 1
+    prod = MealPlan.objects.get(pk=prod_id)
+    customer = request.user.customer
+    user_cart = Cart.objects.get(web_user=user)
+    if user_cart:
+        cart_item = LineItem(quantity=quantity,product=prod,cart=user_cart)
+        cart_item.save()
+    else: # create one
+        new_cart = Cart(user=request.user,
+                        status='Active')
+        new_cart.save()
+        cart_item = LineItem(quantity=quantity,
+                              product=prod,
+                              cart=new_cart)
+        cart_item.save()
+    messages.info(request, f"{prod.name} has been added to your cart.")
+    return render(request, 'cart/index.html')
+
+        
