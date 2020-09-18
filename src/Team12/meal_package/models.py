e5326c5eac2a5059259fc947908033d7f39d479a
ymirthor <ymirthorleifsson@gamil.com>
2020-09-18 16:24:14 +0000

    Added Models for Online Shopping

--- src/Team12/meal_package/models.py
+++ src/Team12/meal_package/models.py
@@ -4,9 +4,11 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 from django.db.models import ManyToManyField
 
-
+""" We have two models ATM commenting this one out
 class Meal_package(models.Model):
     name = models.CharField(max_length=32, blank=False)
     ingredients = models.TextField(blank=False, default="N/A")
     price = models.IntegerField(blank=False)
     recipe = models.TextField(blank=False)
+
+"""
