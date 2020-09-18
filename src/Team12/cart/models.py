6919ec566305ef9e783fd7e76348b88af6225832
fannarl <12133432+fannarl@users.noreply.github.com>
2020-09-18 16:11:58 +0000

    Update models.py

--- src/Team12/cart/models.py
+++ src/Team12/cart/models.py
@@ -1,23 +1,3 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from src.Team12.product.models import MealPlan
-
-
-class CartItem(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True)
-    meal_plan = models.ForeignKey(MealPlan,
-                                null=True,
-                                on_delete=models.SET_NULL)
-    quantity = models.IntegerField(default=1)
-    cart = models.ForeignKey('Cart',
-                             on_delete=models.CASCADE)
-
-# class Contact_info(models.Model):
-#    last_name = models.CharField(max_length=64)
-#    first_name = models.CharField(max_length=64)
-#    country = CountryField()
-#    address = models.CharField(max_length=128)
-#    city = models.CharField(max_length=128)
-#    postcode = models.CharField(max_length=10)
-# Create your models here.
