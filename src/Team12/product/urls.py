efb3fac786fd89089f713624abe0c08fbfd87f5a
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2020-09-19 15:18:27 +0000

    Order By Price Implemented

--- src/Team12/product/urls.py
+++ src/Team12/product/urls.py
@@ -7,5 +7,7 @@ app_name='products'
 urlpatterns = [
     path('', views.index , name='index'),
     path('mealplan/<int:pk>/', views.detailed_product, name='detailed'),
-
+    path('product_order/<str:order>/', views.product_order , name='product_order')
 ]
+
+
