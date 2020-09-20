f6d80d00739d8c42e8e7dbd03014e3714acbbe30
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2020-09-20 18:46:46 +0000

    Comments

--- src/Team12/product/urls.py
+++ src/Team12/product/urls.py
@@ -6,9 +6,9 @@ app_name='products'
 
 urlpatterns = [
     path('', views.index , name='index'),
-    path('filter_price/<int:filter>/<int:filter2>/', views.filter_price, name='filter_price'),
-    path('mealplan/<int:pk>/', views.detailed_product, name='detailed'),
-    path('product_order/<str:order>/', views.product_order , name='product_order'),
+    path('filter_price/<int:filter>/<int:filter2>/', views.filter_price, name='filter_price'), # url takes in two prices 
+    path('mealplan/<int:pk>/', views.detailed_product, name='detailed'), # url takes the primary key of a product
+    path('product_order/<str:order>/', views.product_order , name='product_order'), # url takes in price or -price (the latter meaning descending ordering)
 ]
 
 
--- src/Team12/product/views.py
+++ src/Team12/product/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import MealPlan
 
 def index(request):
-    data = MealPlan.objects.all()
+    data = MealPlan.objects.all() # simple index which draws all meal plans from the database
 
     context = {
         "products": data,
@@ -11,7 +11,7 @@ def index(request):
 
     return render(request, 'products/index.html', context)
 
-def detailed_product(request, pk):
+def detailed_product(request, pk): # A detailed product is genereated by primary key id
     meal_plan = get_object_or_404(MealPlan, pk=pk)
 
     context  = {
@@ -22,7 +22,7 @@ def detailed_product(request, pk):
 
 
 def product_order(request, order):
-    data = MealPlan.objects.order_by(order).all()
+    data = MealPlan.objects.order_by(order).all() # Here the product gets ordered in accending or descending order dependingon the def input var
 
     context = {
         "products": data,
@@ -31,7 +31,7 @@ def product_order(request, order):
     return render(request, 'products/index.html', context)
 
 def filter_price(request, filter, filter2):
-    data = MealPlan.objects.filter(price__gte = filter, price__lte = filter2)
+    data = MealPlan.objects.filter(price__gte = filter, price__lte = filter2) # The mealplan get filtered by two parameters (Greater less and equal)
 
     context = {
         "products": data,
