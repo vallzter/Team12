542539ca522c9f80d0bc79d23f172295363090fb
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2020-10-03 15:11:31 +0000

    more coverage

--- src/Team12/user/tests.py
+++ src/Team12/user/tests.py
@@ -16,77 +16,56 @@ class FrontPageTest(TestCase):
             'username': 'testuser',
             'password': 'secret321'}
         User.objects.create_user(**self.credentials)
+         
 
     def testLogin(self):
-        response = self.client.post('/user/login/', **self.credentials)  
+        response = self.client.post('/user/login/', **self.credentials, follow=True)  
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_profile(self):
-        response = self.client.post('/user/login/', **self.credentials)  
+        response = self.client.post('/user/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
         self.assertEqual(response.status_code, 200)
-        response = self.client.get('profile/')
-        #self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed("profile.html")
-        #response = self.client.get('/user/edit/')
-      #  self.assertEqual(response.status_code, 200)
+        response = self.client.get('/user/register/')
+        self.assertEqual(response.status_code, 200)
+        
 
+
+    def test_editprofile(self):
+        response = self.client.post('/user/login/', self.credentials, follow=True)
+        self.assertTrue(response.context['user'].is_active)
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/user/editProfile/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("user/edit.html")
+        response = self.client.get('/user/edit/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("user/edit.html")
+
+    def test_register(self):
+        response = self.client.get('/user/register/')
+        self.assertTemplateUsed(response, "user/register.html")
 
 class removeUser(TestCase):
     def setUp(self):
         self.credentials = {
-            'username': 'testuser',
-            'password': 'secret321'}
-        User.objects.create_user(**self.credentials)
+            'username': 'testuser2',
+            'password': 'secret3211231'}
+        self.supercredentials = {
+            'username': 'administrator',
+            'password': 'GobleGobleTwinkleGobble123'} 
+        User.objects.create_superuser(**self.supercredentials)
+        response = self.client.post('/admin/', **self.supercredentials)  
+
+
 
     def test_remove_user(self):
-        response = self.client.post('/user/login/', **self.credentials)  
-        response = self.client.post('/user/remove/')  
+        response = self.client.post('/user/login/', **self.supercredentials, follow=True)
+        response = self.client.post('/user/remove/') 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response,"user/user_remove.html")
+        responseExtra = self.client.post('/user/admin/', **self.credentials, follow=True)
+        responseExtra = self.client.post('/user/remove/') 
+        self.assertEqual(responseExtra.status_code, 200)
 
-
-class RegisterTest(TestCase):
-    
-
-    def setUp(self):
-        form = UserCreationForm()
-        #form.cleaned_data.username = "wolfram"
-        #self.assertIsNotNone(form)
-        #form.save()
-        #username = form.cleaned_data.get('username') 
-        #password = form.cleaned_data.get('password1') 
-        #first_name = form.cleaned_data.get('firstname') 
-        #last_name = form.cleaned_data.get('lastname') 
-        #email = form.cleaned_data.get('email')
-
-    def test_register(self):
-        pass
-
-    #def test_homepage(self): #This should return as false until we have a homepage
-        #response = self.client.get('/')
-        #self.assertEqual(response.status_code, 200)
-        #self.assertTemplateUsed("user_page.html")
-        #response = self.client.get('/products')
-        #self.assertEqual(response.status_code, 301)
-        #self.assertTemplateUsed("profile.html")
-
-    #def test_product_site(self):
-        #response = self.client.get('/products')
-        #self.assertEqual(response.status_code, 200)
-        #response = self.client.get('/cart')
-        #self.assertEqual(response.status_code, 301)
-
-    #def test_register(self):
-        #form = UserCreationForm()
-        #self.assertIsNotNone(form)
-
-
-
-        # logs in a dummy user
-        
-        #print(self.user)
-
-        #print(response.content)
-        # asserts if Logout is available
-        #self.assertTrue('Logout' in str(response.content))
