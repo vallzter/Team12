46c4a0257d3670cffb970bc120cb965a1eb5fddb
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2020-10-03 16:04:02 +0000

    more user/view coverage

--- src/Team12/user/tests.py
+++ src/Team12/user/tests.py
@@ -43,10 +43,46 @@ class FrontPageTest(TestCase):
         response = self.client.get('/user/edit/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed("user/edit.html")
+        response = self.client.get('/user/editProfile/')
+        data = {
+            'fname': 'testuser',
+            'lname': 'secret321',
+            'email': 'testuser',
+            'country': 'secret321',
+            'region': 'testuser',
+            'city': '220',
+            'info': 'testuser',
+            'address': 'secret321',
+            'phone': '6616262',
+            'card': '1234567891234567',
+            'cvv': '223',
+            'date': '2222-10-03',
+            'name': 'Hrafnkell Þorri',
+            }
+        response = self.client.post('/user/editProfile/', data)
+        self.assertEqual(response.status_code, 200)
+
+
 
     def test_register(self):
         response = self.client.get('/user/register/')
         self.assertTemplateUsed(response, "user/register.html")
+        self.assertEqual(response.status_code, 200)
+        data = {
+                'username': 'DjangoBoi',
+                'password1': 'secret321',
+                'password2': 'secret321',
+                'firstname': 'testuser',
+                'lastname': 'secret321',
+                'email': 'testuser',
+                }
+        form = UserCreationForm(data)
+        print(form.errors)
+        if form.is_valid():
+            response = self.client.post('/user/register/', data)
+            response = self.client.get('/user/')
+            self.assertEqual(response.status_code, 200)
+        
 
 class removeUser(TestCase):
     def setUp(self):
@@ -57,15 +93,22 @@ class removeUser(TestCase):
             'username': 'administrator',
             'password': 'GobleGobleTwinkleGobble123'} 
         User.objects.create_superuser(**self.supercredentials)
-        response = self.client.post('/admin/', **self.supercredentials)  
 
-
+    def test_remove_super_user(self):
+        response = self.client.post('/admin/', **self.supercredentials, follow=True)
+        response = self.client.get('/user/remove/')
+        self.assertEqual(response.status_code, 200)
+        #self.assertTemplateUsed(response, "user/user_remove.html")
 
     def test_remove_user(self):
-        response = self.client.post('/user/login/', **self.supercredentials, follow=True)
-        response = self.client.post('/user/remove/') 
-        self.assertEqual(response.status_code, 200)
-        responseExtra = self.client.post('/user/admin/', **self.credentials, follow=True)
+        
+        responseExtra = self.client.post('/user/login/', **self.credentials, follow=True)
+        responseExtra = self.client.get('/user/remove/') 
+        self.assertEqual(responseExtra.status_code, 200)
         responseExtra = self.client.post('/user/remove/') 
         self.assertEqual(responseExtra.status_code, 200)
+
+
+
+
 
