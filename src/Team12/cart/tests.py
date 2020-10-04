7da0499448d634f430d04f7182938ef22cccc781
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2020-10-04 11:58:45 +0000

    final for merge

--- src/Team12/cart/tests.py
+++ src/Team12/cart/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class cartTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret321'}
+        User.objects.create_user(**self.credentials)
+         
+        
+    def test_cart(self):
+        response = self.client.post('/user/login/', **self.credentials, follow=True)  
+        response = self.client.get('/cart/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,"/cart/index.html")
